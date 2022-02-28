27f3b8335cc62bb304ea6dda045b1e141be3ca8f
rahidhaque <ontirahid@yahoo.com>
2022-02-28 22:48:23 +0600

    Mobile Phones Displayed

--- js/app.js
+++ js/app.js
@@ -1,9 +1,31 @@
 //getting all the mobile phones
 const allMobiles = () => {
-    const searchValue = document.getElementById('search-box');
+    const searchField = document.getElementById('search-box');
+    const searchValue = searchField.value;
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`;
     fetch(url)
         .then(response => response.json())
-        .then(data => console.log(data.data))
+        .then(datas => showMobiles(datas.data))
 }
 
+//showing all the mobile phones
+const showMobiles = (mobiles) => {
+    for (const mobile of mobiles) {
+        console.log(mobile);
+        const parent = document.getElementById('mobile-container');
+        const div = document.createElement('div');
+        div.classList.add('card');
+        div.classList.add('h-100');
+        div.innerHTML = `                    
+                    <img src="${mobile.image}" class="card-img-top w-50" alt="...">
+                    <div class="card-body m-5">
+                        <h5 class="card-title">Phone Name:</h5>
+                        <p class="card-text">${mobile.phone_name}</p>
+                        <h5 class="card-title">Brand Name:</h5>
+                        <p class="card-text">${mobile.brand}</p>
+                    </div>
+                    </div>
+        `;
+        parent.appendChild(div);
+    }
+}
