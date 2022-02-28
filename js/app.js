1f6f35ef6604b15b631f6ad69e06ed48351a254b
rahidhaque <ontirahid@yahoo.com>
2022-03-01 00:35:24 +0600

    Mobile ID console logged

--- js/app.js
+++ js/app.js
@@ -11,21 +11,25 @@ const allMobiles = () => {
 //showing all the mobile phones
 const showMobiles = (mobiles) => {
     for (const mobile of mobiles) {
-        console.log(mobile);
         const parent = document.getElementById('mobile-container');
         const div = document.createElement('div');
         div.classList.add('card');
         div.classList.add('h-100');
-        div.innerHTML = `                    
+        div.innerHTML = `   
+                    <div class="border">                 
                     <img src="${mobile.image}" class="card-img-top w-50" alt="...">
                     <div class="card-body m-5">
                         <h5 class="card-title">Phone Name:</h5>
                         <p class="card-text">${mobile.phone_name}</p>
                         <h5 class="card-title">Brand Name:</h5>
                         <p class="card-text">${mobile.brand}</p>
+                        <button onclick="mobileDetails('${mobile.slug}')" class="btn btn-success">Details</button>
                     </div>
                     </div>
         `;
         parent.appendChild(div);
     }
+}
+const mobileDetails = (id) => {
+    console.log(id);
 }
