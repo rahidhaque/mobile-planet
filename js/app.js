c24047c1f10f75a0a3181cb48e394deffe0be06f
rahidhaque <ontirahid@yahoo.com>
2022-03-01 01:45:36 +0600

    Error Message for search option added

--- js/app.js
+++ js/app.js
@@ -2,20 +2,33 @@
 const allMobiles = () => {
     const searchField = document.getElementById('search-box');
     const searchValue = searchField.value;
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`;
-    fetch(url)
-        .then(response => response.json())
-        .then(datas => showMobiles(datas.data))
+    searchField.value = "";
+    if (searchValue == "") {
+        document.getElementById('mobile-container').innerHTML = `
+        <h2 class="text-danger text-center">Please Search Something!</h2>
+        `;
+    }
+    else {
+        const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`;
+        fetch(url)
+            .then(response => response.json())
+            .then(datas => showMobiles(datas.data))
+    }
 }
 
 //showing all the mobile phones
 const showMobiles = (mobiles) => {
-    for (const mobile of mobiles) {
-        const parent = document.getElementById('mobile-container');
-        const div = document.createElement('div');
-        div.classList.add('card');
-        div.classList.add('h-100');
-        div.innerHTML = `   
+    const parent = document.getElementById('mobile-container');
+    parent.textContent = "";
+    if (mobiles.length == 0) {
+        parent.innerHTML = `<h2 class="text-danger text-center">No Result Found!</h2>`;
+    }
+    else {
+        for (const mobile of mobiles) {
+            const div = document.createElement('div');
+            div.classList.add('card');
+            div.classList.add('h-100');
+            div.innerHTML = `   
                     <div class="border">                 
                     <img src="${mobile.image}" class="card-img-top w-50" alt="...">
                     <div class="card-body m-5">
@@ -27,9 +40,11 @@ const showMobiles = (mobiles) => {
                     </div>
                     </div>
         `;
-        parent.appendChild(div);
+            parent.appendChild(div);
+        }
     }
 }
+//fetching mobile informations
 const mobileDetails = (id) => {
     const url = `https://openapi.programming-hero.com/api/phone/${id}`;
     fetch(url)
@@ -37,6 +52,23 @@ const mobileDetails = (id) => {
         .then(datas => displayMobileDetails(datas.data))
 }
 
-const displayMobileDetails = (details) => {
-
+//displaying mobile informations
+const displayMobileDetails = (detail) => {
+    document.getElementById('mobile-container').style.display = "none";
+    const parent = document.getElementById('mobile-detail-container');
+    parent.textContent = "";
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.classList.add('h-100');
+    div.classList.add('text-center');
+    console.log(detail.releaseDate);
+    if (detail.releaseDate == '') {
+        detail.releaseDate = "To Be Announced(TBA)";
+    }
+    div.innerHTML = `   
+                    <div class="border">  
+                    <img src="${detail.image}" class="card-img-top w-auto h-auto" alt="...">
+                    < 
+                    `
+    parent.appendChild(div);
 }
